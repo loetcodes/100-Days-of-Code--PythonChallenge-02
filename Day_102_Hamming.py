8a28d243ad9069a11132c33c9d6d14d098be587f
lulets-l <46016065+lulets@users.noreply.github.com>
2020-02-19 22:29:00 +0400

    daily challenge update - exercism

--- Day_102_Hamming.py
+++ Day_102_Hamming.py
@@ -28,8 +28,8 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError('Incorrect length of input')
     count = 0
-    for i in range(len(strand_a)):
-        if strand_a[i] != strand_b[i]:
+    for i,j in zip(strand_a, strand_b):
+        if i != j:
             count += 1
     return count
 
